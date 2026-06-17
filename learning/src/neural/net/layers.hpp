cb6b15ffbf6b727abff4eed136302d9f3fff2744
ferbcastro <fernandocuritiba3@gmail.com>
2026-06-17 22:11:16 +0200

    feat(net): conv layer first (#2)

--- learning/src/neural/net/layers.hpp
+++ learning/src/neural/net/layers.hpp
@@ -1,7 +1,74 @@
-class Layer { };
+#ifndef FACEPGA_LAYERS_HPP_
+#define FACEPGA_LAYERS_HPP_
 
-class Conv : public Layer { };
+#include "tensor.hpp"
+
+#include <torch/torch.h>
+
+// Forward declaration of Loss interface
+class Loss;
+
+class Layer {
+  protected:
+    Tensor* output;
+    Tensor* weights;
+    Tensor* biases;
+
+    friend class Loss;
+  public:
+    Layer() : output(0), weights(0), biases(0) {}
+
+    /** */
+    virtual const Tensor* Forward(Tensor* act) = 0;
+    /** */
+    virtual const Tensor* Backward(Tensor* grad) = 0;
+    /** */
+    virtual void PrintWeights() = 0;
+    /** */
+    virtual void PrintBiases() = 0;
+
+    virtual ~Layer() {
+        if (output) delete[] output;
+        if (weights) delete[] weights;
+        if (biases) delete[] biases;
+    }
+};
+
+enum GroupType {
+    GroupTypeStandard = 0
+};
+
+enum PaddingType {
+    PaddingTypeZeros = 0
+};
+
+class Conv : public Layer {
+  private:
+    int inputChannels;
+    int outputChannels;
+    int kernelSize;
+    int stride;
+    int padding;
+    int dilation;
+
+    GroupType convType;
+    PaddingType paddingType;
+
+    bool hasBias;
+
+  public:
+    /** */
+    virtual const Tensor* Forward(Tensor* activations) { return 0; }
+    /** */
+    virtual const Tensor* Backward(Tensor* gradients) { return 0; }
+    /** */
+    virtual void PrintWeights() {}
+    /** */
+    virtual void PrintBiases() {}
+};
 
 class Pool : public Layer { };
 
 class Dense : public Layer { };
+
+#endif
